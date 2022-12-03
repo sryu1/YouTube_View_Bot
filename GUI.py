abf01b87bdf6345bfb9639cf95ff6ffcbc3bd9cb
sryu1 <95025816+sryu1@users.noreply.github.com>
2022-12-03 21:54:40 +1000

    Attempt to add settings.

--- GUI.py
+++ GUI.py
@@ -1,3 +1,4 @@
+import os
 import time
 import random
 import tkinter
@@ -6,6 +7,47 @@ import json
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 import chromedriver_autoinstaller
+
+
+def config():
+    if not os.path.isfile('config.json'):
+        configs = {
+            "Headless": headless.get(),
+            "Mute": mute.get()
+        }
+        json_file = json.dumps(configs)
+        with open("config.json", "w") as jsonfile:
+            jsonfile.write(json_file)
+
+    else:
+        with open("config.json", "r") as jsonfile:
+            json_options = json.load(jsonfile)
+            jsonfile.close()
+
+        def hdls():
+
+            if json_options["Headless"] == 1:
+                headless.select()
+            else:
+                headless.deselect()
+
+        def sdop():
+
+            if json_options["Mute"] == 1:
+                mute.select()
+            else:
+                mute.deselect()
+
+        configs = {
+            "Headless": headless.get(),
+            "Mute": mute.get()
+        }
+        json_file = json.dumps(configs)
+        with open("config.json", "w") as jsonfile:
+            jsonfile.write(json_file)
+        hdls()
+        sdop()
+
 
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
@@ -28,11 +70,11 @@ frame_1 = customtkinter.CTkFrame(master=app)
 frame_1.pack(pady=20, padx=20, fill="both", expand=True)
 
 # Checkbox
-checkbox_1 = customtkinter.CTkCheckBox(master=frame_1, text="Headless Mode")
-checkbox_1.pack(pady=10, padx=10)
+headless = customtkinter.CTkCheckBox(master=frame_1, text="Headless Mode", command=config)
+headless.pack(pady=10, padx=10)
 
-checkbox_2 = customtkinter.CTkCheckBox(master=frame_1, text="Mute")
-checkbox_2.pack(pady=10, padx=10)
+mute = customtkinter.CTkCheckBox(master=frame_1, text="Mute", command=config)
+mute.pack(pady=10, padx=10)
 
 # Number of views
 slider_1 = customtkinter.CTkSlider(master=frame_1, command=slider_callback, from_=0, to=1)
@@ -58,5 +100,6 @@ button_1.pack(pady=10, padx=10)
 # Bot Progress
 progressbar_1 = customtkinter.CTkProgressBar(master=frame_1)
 progressbar_1.pack(pady=10, padx=10)
+progressbar_1.set(0.0)
 
 app.mainloop()
