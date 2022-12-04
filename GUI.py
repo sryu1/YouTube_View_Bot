5f198618351a8a7cfbc62e4987ad1c522e70cc4e
sryu1 <95025816+sryu1@users.noreply.github.com>
2022-12-04 22:05:18 +1000

    Added settings. Some fixes.

--- GUI.py
+++ GUI.py
@@ -8,6 +8,13 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 import chromedriver_autoinstaller
 
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+app = customtkinter.CTk()
+app.geometry("800x600")
+app.title("YouTube View Bot")
+
 
 def config():
     if not os.path.isfile('config.json'):
@@ -19,50 +26,29 @@ def config():
         with open("config.json", "w") as jsonfile:
             jsonfile.write(json_file)
 
-    else:
-        with open("config.json", "r") as jsonfile:
-            json_options = json.load(jsonfile)
-            jsonfile.close()
 
-        def hdls():
-
-            if json_options["Headless"] == 1:
-                headless.select()
-            else:
-                headless.deselect()
-
-        def sdop():
-
-            if json_options["Mute"] == 1:
-                mute.select()
-            else:
-                mute.deselect()
-
-        configs = {
-            "Headless": headless.get(),
-            "Mute": mute.get()
-        }
-        json_file = json.dumps(configs)
-        with open("config.json", "w") as jsonfile:
-            jsonfile.write(json_file)
-        hdls()
-        sdop()
+with open("config.json", "r") as jsonfile:
+    json_options = json.load(jsonfile)
+    jsonfile.close()
 
 
-customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("blue")
-
-app = customtkinter.CTk()
-app.geometry("800x600")
-app.title("YouTube View Bot")
-
-
-def button_callback():
-    print("Start Bot")
+def write_settings():
+    configs = {
+        "Headless": headless.get(),
+        "Mute": mute.get()
+    }
+    json_file = json.dumps(configs)
+    with open("config.json", "w") as jsonfile:
+        jsonfile.write(json_file)
 
 
 def slider_callback(value):
-    print(slider_1.get())
+    views = int(view_slider.get())
+    print(views)
+
+
+def start_bot():
+    write_settings()
 
 
 # Border
@@ -72,34 +58,44 @@ frame_1.pack(pady=20, padx=20, fill="both", expand=True)
 # Checkbox
 headless = customtkinter.CTkCheckBox(master=frame_1, text="Headless Mode", command=config)
 headless.pack(pady=10, padx=10)
+if json_options["Headless"] == 1:
+    headless.select()
+else:
+    headless.deselect()
 
 mute = customtkinter.CTkCheckBox(master=frame_1, text="Mute", command=config)
 mute.pack(pady=10, padx=10)
+if json_options["Mute"] == 1:
+    mute.select()
+else:
+    mute.deselect()
 
 # Number of views
-slider_1 = customtkinter.CTkSlider(master=frame_1, command=slider_callback, from_=0, to=1)
-slider_1.pack(pady=10, padx=10)
-slider_1.set(0.4)
+view_number = customtkinter.CTkLabel(master=frame_1, justify=tkinter.LEFT, text="Number")
+view_number.pack(pady=10, padx=10)
+view_slider = customtkinter.CTkSlider(master=frame_1, command=slider_callback, from_=1, to=50)
+view_slider.pack(pady=10, padx=10)
+view_slider.set(20)
 
 # Number of tabs
-entry_2 = customtkinter.CTkEntry(master=frame_1, width=200, placeholder_text="Number of tabs")
-entry_2.pack(pady=10, padx=10)
+tab_box = customtkinter.CTkEntry(master=frame_1, width=200, placeholder_text="Number of tabs")
+tab_box.pack(pady=10, padx=10)
 
 # Watch time
-entry_3 = customtkinter.CTkEntry(master=frame_1, width=200, placeholder_text="Watch time (seconds)")
-entry_3.pack(pady=10, padx=10)
+watchtime_box = customtkinter.CTkEntry(master=frame_1, width=200, placeholder_text="Watch time (seconds)")
+watchtime_box.pack(pady=10, padx=10)
 
 # Link
-entry_4 = customtkinter.CTkEntry(master=frame_1, width=400, placeholder_text="Link")
-entry_4.pack(pady=10, padx=10)
+link_box = customtkinter.CTkEntry(master=frame_1, width=400, placeholder_text="Link")
+link_box.pack(pady=10, padx=10)
 
 # Start Button
-button_1 = customtkinter.CTkButton(master=frame_1, command=button_callback, text="Start Bot")
+button_1 = customtkinter.CTkButton(master=frame_1, command=start_bot, text="Start Bot")
 button_1.pack(pady=10, padx=10)
 
 # Bot Progress
-progressbar_1 = customtkinter.CTkProgressBar(master=frame_1)
-progressbar_1.pack(pady=10, padx=10)
-progressbar_1.set(0.0)
+progress = customtkinter.CTkProgressBar(master=frame_1)
+progress.pack(pady=10, padx=10)
+progress.set(0.0)
 
 app.mainloop()
