169eccf324dbeb9c5ff4859d72e8a163f0393e36
sryu1 <95025816+sryu1@users.noreply.github.com>
2022-12-05 09:48:04 +1000

    Functional GUI Bot

--- GUI.py
+++ GUI.py
@@ -1,9 +1,9 @@
-import os
 import time
 import random
 import tkinter
 import customtkinter
 import json
+import threading
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 import chromedriver_autoinstaller
@@ -16,20 +16,6 @@ def main():
     app = customtkinter.CTk()
     app.geometry("800x600")
     app.title("YouTube View Bot")
-
-    drivers = []
-    sites = ['https://search.yahoo.com/', 'https://duckduckgo.com/', 'https://www.google.com/',
-             'https://www.bing.com/', 'https://t.co/', 'https://youtube.com']
-
-    def config():
-        if not os.path.isfile('config.json'):
-            configs = {
-                "Headless": headless.get(),
-                "Mute": mute.get()
-            }
-            json_file = json.dumps(configs)
-            with open("config.json", "w") as jsonfile:
-                jsonfile.write(json_file)
 
     with open("config.json", "r") as jsonfile:
         json_options = json.load(jsonfile)
@@ -44,24 +30,30 @@ def main():
         with open("config.json", "w") as jsonfile:
             jsonfile.write(json_file)
 
-    def slider_callback(value):
-        global views
+    def progress_bar():
+        viewcount = open("Bot Status/viewcount.txt", "r")
+        wsviewcount = int(viewcount.read())
+        viewcount.close()
         views = int(view_slider.get())
+        progress.set(wsviewcount / views)
 
     def start_bot():
         write_settings()
         chromedriver_autoinstaller.install()
         viewcount = 0
-        print(views)
-        number_of_drivers = tab_box.get()
-        print(number_of_drivers)
-        time_to_refresh = watchtime_box.get()
-        print(time_to_refresh)
+
+        drivers = []
+        sites = ['https://search.yahoo.com/', 'https://duckduckgo.com/', 'https://www.google.com/',
+                 'https://www.bing.com/', 'https://t.co/', 'https://youtube.com']
+        views = int(view_slider.get())
+        number_of_drivers = int(tab_box.get())
+        time_to_refresh = int(watchtime_box.get())
         url = link_box.get()
-        print(url)
+
         wsviews = open("Bot Status/views.txt", 'w')
         wsurl = open("Bot Status/url.txt", 'w')
-        wsviews.write(views)
+
+        wsviews.write(str(views))
         wsviews.close()
         wsurl.write(url)
         wsurl.close()
@@ -81,8 +73,6 @@ def main():
             if json_options["Headless"] == 1:
                 options.add_argument("--headless")
             if json_options["Mute"] == 1:
-                pass
-            else:
                 options.add_argument("--mute-audio")
             options.add_experimental_option("excludeSwitches", ["enable-logging"])
             drivers.append(webdriver.Chrome(options=options,
@@ -95,13 +85,10 @@ def main():
             time.sleep(time_to_refresh)
             viewcount += 1
             wsviewcount()
+            progress_bar()
 
-            print("view count = " + str(viewcount))
             if int(views) <= int(viewcount):
                 drivers[i].quit()
-                print("the listed amount has been viewed")
-                os.system("pause")
-                exit()
             elif int(views) > int(viewcount):
                 for i in range(number_of_drivers):
                     drivers[i].refresh()
@@ -112,24 +99,26 @@ def main():
     border.pack(pady=20, padx=20, fill="both", expand=True)
 
     # Checkbox
-    headless = customtkinter.CTkCheckBox(master=border, text="Headless Mode", command=config)
+    headless = customtkinter.CTkCheckBox(master=border, text="Headless Mode")
     headless.pack(pady=10, padx=10)
     if json_options["Headless"] == 1:
         headless.select()
     else:
         headless.deselect()
 
-    mute = customtkinter.CTkCheckBox(master=border, text="Mute", command=config)
+    mute = customtkinter.CTkCheckBox(master=border, text="Mute")
     mute.pack(pady=10, padx=10)
     if json_options["Mute"] == 1:
         mute.select()
     else:
         mute.deselect()
-
+    # Bot Progress
+    progress = customtkinter.CTkProgressBar(master=border)
+    progress.set(0.0)
     # Number of views
-    view_number = customtkinter.CTkLabel(master=border, justify=tkinter.LEFT, text="Number")
+    view_number = customtkinter.CTkLabel(master=border, justify=tkinter.LEFT, text="Views (1-50)")
     view_number.pack(pady=10, padx=10)
-    view_slider = customtkinter.CTkSlider(master=border, command=slider_callback, from_=1, to=50)
+    view_slider = customtkinter.CTkSlider(master=border, from_=1, to=50)
     view_slider.pack(pady=10, padx=10)
     view_slider.set(20)
 
@@ -144,11 +133,10 @@ def main():
     # Link
     link_box = customtkinter.CTkEntry(master=border, width=400, placeholder_text="Link")
     link_box.pack(pady=10, padx=10)
-    # Bot Progress
-    progress = customtkinter.CTkProgressBar(master=border)
-    progress.set(0.0)
+
     # Start Button
-    button_1 = customtkinter.CTkButton(master=border, command=start_bot, text="Start Bot")
+    button_1 = customtkinter.CTkButton(master=border, command=threading.Thread(target=start_bot).start,
+                                       text="Start Bot")
     button_1.pack(pady=10, padx=10)
 
     progress.pack(pady=10, padx=10)
