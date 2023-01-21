3d5e9764c123545456b2328d86bf399588110863
sryu1 <95025816+sryu1@users.noreply.github.com>
2023-01-21 19:59:07 +1000

    Updates to killprocess

--- killprocess.bat
+++ killprocess.bat
@@ -5,3 +5,5 @@ echo THIS WILL CLOSE ALL CHROME INSTANCES (INCLUDING CHROME BROWSER)
 pause
 taskkill /F /IM chrome.exe
 taskkill /F /IM chromedriver.exe
+echo All chrome processes has been terminated.
+pause
